6f725352e4b0a63bdf31f7a2e2d15fc121fdb9bf
miguelb <miguelbermudezestrada@gmail.com>
2026-01-07 13:32:26 -0600

    Agrega índice de contenido al inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,32 @@
 export default function HomePage() {
   return (
     <main>
-      <h1>Bienvenidos al inicio de mi proyecto</h1>
-      <p>El objetivo de este proyecto es practicar el manejo de y creacion de aplicaciones</p>
-      <p>Dicho blog sera sobre comics los cuales me apacionan y se les hablara un poco sobre ellos</p>
+      {/* Índice */}
+      <nav>
+        <h2>Índice</h2>
+        <ul>
+          <li><a href="#inicio">Inicio</a></li>
+          <li><a href="#objetivo">Objetivo del proyecto</a></li>
+          <li><a href="#tema">Tema del blog</a></li>
+        </ul>
+      </nav>
+
+      {/* Contenido */}
+      <section id="inicio">
+        <h1>Bienvenidos al inicio de mi proyecto</h1>
+      </section>
+
+      <section id="objetivo">
+        <p>
+          El objetivo de este proyecto es practicar el manejo y creación de aplicaciones
+        </p>
+      </section>
+
+      <section id="tema">
+        <p>
+          Dicho blog será sobre cómics
+        </p>
+      </section>
     </main>
   );
 }
